f8d4abd1ce8176e13f053c3a4227d9dfef1c7564
nando <muhammad.rezki.ananda@gmail.com>
2021-04-02 17:43:00 +0700

    INITIAL COMMIT

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        body: Container(
+          margin: EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              buildCard(Icons.account_balance_wallet_sharp, "Account Box"),
+              buildCard(Icons.adb, "Serangga Android")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Card buildCard(IconData iconData, String text) {
+    return Card(
+      elevation: 5,
+      child: Row(
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.all(5),
+              child: Icon(iconData, color: Colors.green)),
+          Text(text)
+        ],
+      ),
+    );
+  }
+}
